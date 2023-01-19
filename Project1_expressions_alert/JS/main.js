e55580b3fe4e3b9ad2c640f5ef0cd78f0f08bc91
mglissmeyer <117939547+mglissmeyer@users.noreply.github.com>
2023-01-19 12:03:35 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+document.write('Hello, World!'); //Hello World is on the page
+
+var Family = "The Bat Family", Bruce = "Batman", Richard = "Nightwing", Tim = "Robin", Barbara = "Oracle"; //Defining a variable
+document.write (Bruce); //The value Bruce will display on the page
+
+document.write(3+3); //Writing a expression to show on the page
+
+window.alert('Hello, World!'); //Hello, World will pop up on the page
+
+var Sent1 = "This is the beginning of the string" //Defining a variable
+var Sent2 = " and this is the end of the string" //Defining a variable
+document.write(Sent1); //Sent1 will be written on the page
+document.write(Sent2); //Sent2 will be written on the page
